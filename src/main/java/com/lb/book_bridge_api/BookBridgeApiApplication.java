0f1051bc398256ccb38c759b50fc53c71c710250
Lasha Less <lashawilldata@gmail.com>
2025-03-14 19:25:58 +0100

    initial commit

---
+++ src/main/java/com/lb/book_bridge_api/BookBridgeApiApplication.java
@@ -0,0 +1,13 @@
+package com.lb.book_bridge_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBridgeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBridgeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lb/book_bridge_api/BookBridgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lb.book_bridge_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBridgeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
